69eeb4f1af6194e4529e0839fdcbca6316f4406b
Lucas <72617059+Lukynha15@users.noreply.github.com>
2025-03-19 18:22:44 -0300

    exercicio_one.js

--- Desafio/exercicio_one.js
+++ Desafio/exercicio_one.js
@@ -8,11 +8,13 @@ let sum = numberOne + numberTwo
 let sub = numberOne - numberTwo
 let mult = numberOne * numberTwo
 let half = numberOne / numberTwo
+let rest = numberOne % numberTwo
 
 alert(`A soma dos números foi ${sum}`)
 alert(`A subtração dos números foi ${sub}`)
 alert(`A multiplicação dos números foi ${mult}`)
 alert(`A divisão dos números foi ${half.toFixed(2)}`)
+alert(`O resto da divisão dos números foi ${rest}`)
 
 if ( sum % 2 === 0 ) {
   alert("A soma dos números foi par")
